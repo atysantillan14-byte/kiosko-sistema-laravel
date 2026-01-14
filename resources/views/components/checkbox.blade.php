9a9ae0e0cb3df5403937e09afe23c3581249e438
atysantillan14-byte <atysantillan14@gmail.com>
2026-01-14 04:09:05 -0300

    Add checkbox blade component

---
+++ resources/views/components/checkbox.blade.php
@@ -0,0 +1,9 @@
+@props(['disabled' => false])
+
+<input
+    type="checkbox"
+    {{ $disabled ? 'disabled' : '' }}
+    {{ $attributes->merge([
+        'class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500',
+    ]) }}
+>
