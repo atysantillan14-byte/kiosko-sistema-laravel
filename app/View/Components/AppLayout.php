9b4f75396c0f3a7dc8a6ff01bb1ba6af4df0e448
atysantillan14-byte <atysantillan14@gmail.com>
2026-01-15 16:34:29 -0300

    Fix home layout theme and navbar

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -7,6 +7,23 @@ use Illuminate\View\View;
 
 class AppLayout extends Component
 {
+    public bool $hideNav;
+    public bool $fullBleed;
+    public string $bodyClass;
+    public string $shellClass;
+
+    public function __construct(
+        bool $hideNav = false,
+        bool $fullBleed = false,
+        string $bodyClass = '',
+        string $shellClass = 'min-h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-white via-slate-50 to-slate-100'
+    ) {
+        $this->hideNav = $hideNav;
+        $this->fullBleed = $fullBleed;
+        $this->bodyClass = $bodyClass;
+        $this->shellClass = $shellClass;
+    }
+
     /**
      * Get the view / contents that represents the component.
      */
